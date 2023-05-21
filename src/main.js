b8523619b54b9b1cc2d539656206010b6fdae0b7
Yizhen Jiang <2540667841@qq.com>
2023-05-22 01:19:27 +0800

    add predict (incomplete)

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import * as echarts from "echarts"
 import dataV from '@jiaminghi/data-view'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+
 // import L from 'leaflet';
 // import 'leaflet-velocity/dist/leaflet-velocity.css';
 // import 'leaflet-velocity/dist/leaflet-velocity';
@@ -10,6 +14,7 @@ import router from './router'
 Vue.config.productionTip = false
 Vue.prototype.$echarts = echarts
 Vue.use(dataV)
+Vue.use(ElementUI)
 
 new Vue({
   router,

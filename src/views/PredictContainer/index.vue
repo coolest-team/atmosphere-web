057a0ccb1ee10b0555825c0e16c6d5f042430cec
MintChoco5 <wutt0509@gmail.com>
2023-05-21 17:13:03 +0800

    change prediction page and wind page

---
+++ src/views/PredictContainer/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="parent">
+    <div class="div1">
+      <small-title title="1" />啊啊啊
+    </div>
+    <div class="div2">
+      <small-title title="2" />啊啊啊
+    </div>
+    <div class="div3">
+      <small-title title="3" />啊啊啊
+    </div>
+  </div>
+</template>
+
+<script>
+import smallTitle from "@/components/smallTitle";
+export default {
+  components: {
+    smallTitle
+  }
+};
+</script>
+
+<style scoped>
+.parent {
+  display: grid;
+  width: 100%;
+  height: 100%;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 5px;
+  grid-row-gap: 5px;
+}
+
+.div1 {
+  grid-area: 1 / 1 / 3 / 2;
+  background-color: whitesmoke;
+  box-shadow: 0 2px 2px 0 rgba(67, 67, 67, 0.2),
+    0 6px 20px 0 rgba(80, 80, 80, 0.19);
+  border-radius: 3px;
+}
+.div2 {
+  grid-area: 1 / 2 / 2 / 3;
+  background-color: whitesmoke;
+  box-shadow: 0 2px 2px 0 rgba(67, 67, 67, 0.2),
+    0 6px 20px 0 rgba(80, 80, 80, 0.19);
+  border-radius: 3px;
+}
+.div3 {
+  grid-area: 2 / 2 / 3 / 3;
+  background-color: whitesmoke;
+  box-shadow: 0 2px 2px 0 rgba(67, 67, 67, 0.2),
+    0 6px 20px 0 rgba(80, 80, 80, 0.19);
+  border-radius: 3px;
+}
+</style>
--- src/views/WindContainer/index.vue
+++ src/views/WindContainer/index.vue
@@ -1,14 +1,16 @@
 <template>
   <a-spin :spinning="spinning" size="small">
-    <div id="map" style="margin:0 auto;width: 100%;height: 800px;"></div>
+    <div id="map" style="margin:0 auto;width: 100%;height: 640px;"></div>
   </a-spin>
 </template>
 
 <script>
 import L from "leaflet";
-import 'leaflet/dist/leaflet.css'
+import "leaflet/dist/leaflet.css";
 import "leaflet-velocity/dist/leaflet-velocity.css";
 import "leaflet-velocity/dist/leaflet-velocity";
+import "leaflet.chinatmsproviders";
+import datas from "./wind-global.json";
 //  注：this.datas为风场数据 格式类型可参照wind-js-server 的grib2json格式
 //  至此结束，风场数据已经可以展示在地图中！
 //  有什么不理解的地方或者报错的地方欢迎留言
@@ -18,11 +20,15 @@ export default {
   },
   methods: {
     initMap() {
-      var Esri_DarkGreyCanvas = L.tileLayer(
-        "http://{s}.sm.mapstack.stamen.com/" +
-        "(toner-lite,$fff[difference],$fff[@23],$fff[hsl-saturation@20])/" +
-        "{z}/{x}/{y}.png"
-      );
+      // var Esri_WorldImagery = L.tileLayer(
+      //   "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+      //   {
+      //     attribution:
+      //       "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, " +
+      //       "AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+      //   }
+      // );
+      var normalm = L.tileLayer.chinaProvider("Google.Normal.Map");
       //生成风场实例
       var velocityLayer = L.velocityLayer({
         displayValues: true,
@@ -31,7 +37,7 @@ export default {
           displayPosition: "bottomleft",
           displayEmptyString: "No wind data"
         },
-        //   data: this.datas, //风场数据
+        data: datas, //风场数据
         minVelocity: 0, //Velocity：速率
         maxVelocity: 10,
         velocityScale: 0.005,
@@ -48,12 +54,39 @@ export default {
       });
       //添加风场样式至地图中
       this.map = L.map("map", {
-        center: [38.065, 114.61],
-        zoom: 13,
-        layers: [Esri_DarkGreyCanvas]
+        center: [33.59, 107.29],
+        zoom: 5,
+        maxZoom: 20,
+        minZoom: 5,
+        layers: [normalm]
       });
       // 风场实例添加到地图上
       velocityLayer.addTo(this.map);
+      // var velocityLayer = L.velocityLayer({
+      //   displayValues: true,
+      //   displayOptions: {
+      //     velocityType: "Global Wind",
+      //     position: "bottomleft",
+      //     emptyString: "No wind data"
+      //   },
+      //   data: datas,
+      //   maxVelocity: 15
+      // });
+      // layerControl.addOverlay(velocityLayer, "Wind - Global");
+      // $.getJSON("wind-global.json", function(data) {
+      //   var velocityLayer = L.velocityLayer({
+      //     displayValues: true,
+      //     displayOptions: {
+      //       velocityType: "Global Wind",
+      //       position: "bottomleft",
+      //       emptyString: "No wind data"
+      //     },
+      //     data: data,
+      //     maxVelocity: 15
+      //   });
+
+      //   layerControl.addOverlay(velocityLayer, "Wind - Global");
+      // });
     }
   }
 };
